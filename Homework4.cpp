7bd5b24e5237480d797a569eb0157692a51a6e32
chetmichals <chetmichals@Gmail.com>
2015-03-24 02:48:38 -0400

    Updated code for homework 4

--- Homework4.cpp
+++ Homework4.cpp
@@ -1,8 +1,8 @@
-/*Code is a simple simulation of the Birthday Problem, in order to calculate the average number of people needed to be asked 
-to have two with the same Birthday
-
+/*
 Written by Chet Michals
-Created 01/15/2015
+Created 03/23/15
+
+Used to show the difference in Variance and probability of different distribution functions 
 */
 
 #include <iostream>
@@ -27,15 +27,17 @@ int main()
 	for (int simulationCount = 1; simulationCount <= 10000; simulationCount++)
 	{
 		
+		//Uncomment section for each diffrent distrabtuion
 
 		// case 1
 		float X1 = (float)distribution(generator) / 10000000;
 		float X2 = (float)distribution(generator) / 10000000;
 
-		/* 
+		
+		 
 		
 		//Used for case 2
-
+		/*
 		float z = (float)distribution(generator) / 10000000;
 		float X1 = -1*log(1-z)/2;
 
@@ -44,19 +46,32 @@ int main()
 
 		*/
 
-		/*case 3
+		//case 3
+		/*
 		float X1 = (float)distribution(generator) / 10000000; //Distrabution from 0 to 1
 		float X2 = (float)distribution(generator) / 5000000; //Distrabution from 0 to 2
 		*/
 
 		
-		/*
+		
 		//case 4
+		/*
 		float X1 = (float)distribution(generator) / 10000000;
 		X1 = (X1 > .2) ? .3 : 1.3; // Whooooo, A conditional operator. I don't think I'll ever use one of these again, too hard to read
+		
+		float X2 = (float)distribution(generator) / 10000000;
+		X2 = (X2 > .2) ? .3 : 1.3; // Whooooo, A conditional operator. I don't think I'll ever use one of these again, too hard to read
 		*/
+
+		//case4 v2
+		/*
+		float X1 = (float)distribution(generator) / 10000000;
+		if (X1 > .2 ) X1 = .3; else X1 = 1.3;
 		
-		
+		float X2 = (float)distribution(generator) / 10000000;
+		if (X2 > .2 ) X2 = .3; else X2 = 1.3;
+		*/
+
 		float Y = X1 + X2;
 		S1 = S1 + Y;
 		S2 = S2 + (Y * Y);
